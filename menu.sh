3f608bfea2a531227fd9cd4c1684dd0ae05e1755
Pemula1 <59829400+Pemula1@users.noreply.github.com>
2020-01-19 15:12:50 +0700

    Add files via upload

---
+++ menu.sh
@@ -0,0 +1,187 @@
+cp menu.sh /data/data/com.termux/files/home
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+
+ulang="y"
+while [ $ulang = "y" ];
+do
+echo $i"██████▀▀▀░░░░▀▀▀██████     $cy╔═══════════════════════════╗"
+echo $i"███▀░░░░░░░░░░░░░░▀███     $cy║$ku Author : Tito       ║"
+echo $i"██│░░░░░░░░░░░░░░░░│██     $cy║$ku Pemula1              ║"
+echo $i"█▌│░░░░░░░░░░░░░░░░│▐█     $cy║$ku Tools Caping          ║"
+echo $i"█░└┐░░░░░░░░░░░░░░░┌┘█     $cy╚═══════════════════════════╝"
+echo $i"█░░└┐░░░░░░░░░░░░░┌┘░█"
+echo $i"█░░┌┘▄▄▄▄░░░░▄▄▄▄▄└┐░█"
+echo $i"▌░│████▌░░░▐█████│░▐ █    $cy ╔═══════════════════════════╗"
+echo $i"█░│▐█▀▀░░▄░░▀▀██▌│░███    $cy ║$me WELCOME TO TOOLS PEMULA1$cy   ║"
+echo $i"█▀─┘░░░░░▐█▌░░░░░░└─▀█    $cy ║$me KALIAN HARUS PILIH NO 0 (NOL) DAHULU$cy ║"
+echo $i"█▄░░▄▄▓░░▀█▀░░▓▄▄▄░░▄█    $cy ║$me JIKA TIDAK AKAN TERJADI MASALAH$cy ║"
+echo $i"█▄─┘██▌░░░░░░░░▐██└─▄█    $cy ║$me SELAMAT MENCOBA $cy              ║"
+echo $i"██░░▐█─┬┬┬┬┬┬┬┬─█▌░░██    $cy ║$me SEMOGA BERMANFAAT $cy          ║"
+echo $i"█▌░░░▀🦂┼🦂┼🦂┼🦂█ ███    $cy ╚═══════════════════════════╝"
+echo $i"██▄░░░└┴┴┴┴┴┴┴┴┘░░▄███"
+echo $i"███▄░░░░░░░░░░░░▄█████"
+echo $i"███████▄▄▄▄▄▄▄████████"
+
+echo $ku" WARRING !!!!!!!"
+echo $me" MOHON DI KONDISIKAN BOS "
+echo $me" APAPUN YANG TERJADI BUKAN TANGGUNG JAWAB KAMI LAGI "
+echo "      "
+echo "SCRIPT YANG TERSEDIA" |lolcat
+echo "##############################" |lolcat
+echo $pur" 0.PENDUKUNG AGAR WORK "
+echo "##############################" |lolcat
+echo "\033[1;33m (1).CAPING 1"
+echo "----------------------------------------------" |lolcat
+echo "\033[1;33m (2).CAPING 2"
+echo "----------------------------------------------" |lolcat
+echo "\033[1;33m (3).CAPING 4"
+echo "----------------------------------------------"  |lolcat
+echo "\033[1;33m (4).CAPING 5"
+echo "----------------------------------------------"  |lolcat
+echo "\033[1;33m (5).CAPING 5"
+echo "----------------------------------------------"  |lolcat
+echo "\033[1;33m (6).CAPING 6"
+echo "----------------------------------------------"  |lolcat
+echo "\033[1;33m (7).CAPING 7"
+echo "----------------------------------------------"  |lolcat
+echo "\033[1;33m (8).CAPING 8"
+echo "----------------------------------------------"  |lolcat
+echo "\033[1;33m (9).CAPING 9"
+echo "----------------------------------------------"  |lolcat
+echo "\033[1;33m (10).CAPING 10"
+echo "----------------------------------------------" |lolcat
+echo "\033[1;36m (99).EXIT PROGAM"
+echo "----------------------------------------------" |lolcat
+sleep 0.2
+read -p "Masukan pilihan anda :" pilih;
+
+if [ $pilih = 1 ]; then
+    echo "loding caping 1....."
+    cd $HOME
+    cd ping/data/caping1
+    php caping1.php
+exit 0
+elif [ $pilih = 2 ]; then
+    echo "loding caping 2....."
+    cd $HOME
+    cd ping/data/caping1
+    cp keys /data/data/com.termux/files/home/ping/data/caping2
+    cd $HOME
+    cd ping/data/caping2
+    php caping2.php
+exit 0
+elif [ $pilih = 3 ]; then
+    echo "loding caping 3....."
+    cd $HOME
+    cd ping/data/caping1
+    cp keys /data/data/com.termux/files/home/ping/data/caping3
+    cd $HOME
+    cd ping/data/caping3
+    php caping3.php
+exit 0
+elif [ $pilih = 4 ]; then
+    echo "loding caping 4....."
+    cd $HOME
+    cd ping/data/caping1
+    cp keys /data/data/com.termux/files/home/ping/caping/caping4
+    cd $HOME
+    cd ping/data/caping4
+    php caping4.php
+exit 0
+elif [ $pilih = 5 ]; then
+    echo "loding caping 5....."
+    cd $HOME
+    cd ping/data/caping1
+    cp keys /data/data/com.termux/files/home/ping/data/caping5
+    cd $HOME
+    cd ping/data/caping5
+    php caping5.php
+exit 0
+elif [ $pilih = 6 ]; then
+  echo "loding caping 6....."
+    cd $HOME
+    cd ping/data/caping1
+    cp keys /data/data/com.termux/files/home/ping/data/caping6
+    cd $HOME
+    cd ping/data/caping6
+    php caping6.php
+exit 0
+elif [ $pilih = 7 ]; then
+    echo "loding caping 7....."
+    cd $HOME
+    cd ping/data/caping1
+    cp keys /data/data/com.termux/files/home/ping/data/caping7
+    cd $HOME
+   cd ping/data/caping7
+    php caping7.php
+exit 0
+elif [ $pilih = 8 ]; then
+    echo "loding caping 8....."
+    cd $HOME
+    cd ping/data/caping1
+    cp keys /data/data/com.termux/files/home/ping/data/caping8
+    cd $HOME
+    cd ping/data/caping8
+    php caping8.php
+exit 0
+elif [ $pilih = 9 ]; then
+    echo "loding caping 9....."
+    cd $HOME
+    cd ping/data/caping1
+    cp keys /data/data/com.termux/files/home/ping/data/caping9
+    cd $HOME
+    cd ping/data/caping9
+    php caping9.php
+exit 0
+elif [ $pilih = 10 ]; then
+    echo "loding caping 10....."
+    cd $HOME
+    cd ping/data/caping1
+    cp keys /data/data/com.termux/files/home/ping/data/caping10
+    cd $HOME
+    cd ping/data/caping10
+    php caping10.php
+exit 0
+elif [ $pilih = 0 ]; then
+
+   clear
+   pkg update -y
+clear
+   pkg upgrade -y
+clear
+   pkg install php -y
+clear
+   pip install nano -y
+clear
+   pip2 install git
+clear
+   pkg install python -y
+clear
+   pkg install python2 -y
+clear
+   pkg install python3 -y
+clear
+   pkg install bash -y
+clear
+else
+   echo "\033[1;31m WRONG INPUT!!"
+   echo "HARAP MASUKAN INPUT DENGAN BENAR"
+   echo $ulang
+fi
+done
